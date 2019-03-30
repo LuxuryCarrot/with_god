effd9a8c93ccbea84d44d73ebc8aa4b1b9714741
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-03-30 23:45:30 +0900

    점프 구현중

--- With_God_project/Assets/Script/PlayerFSMManager.cs
+++ With_God_project/Assets/Script/PlayerFSMManager.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public enum PlayerState
 {
     IDLE = 0,
-
     RUN,
     JUMP,
     DEAD
@@ -17,17 +16,25 @@ public class PlayerFSMManager : MonoBehaviour {
     public CharacterController cc;
     public float moveSpeed;
     public float fallSpeed;
+    public float jump;
     public float xMove;
+
+    
     public Animator animator;
     public SpriteRenderer mySpriteRenderer;
 
+    private Vector2 moveDirection = Vector2.zero;
+    //public Vector2 gravity = new Vector3(0, -10);
     bool isJumping;
+
 
     Dictionary<PlayerState, PlayerFSMState> states = new Dictionary<PlayerState, PlayerFSMState>();
 
     private void Awake()
     {
         moveSpeed = 0.5f;
+        jump = 10.0f;
+        fallSpeed = -20.0f;
         cc = GetComponent<CharacterController>();
 
         animator = GetComponent<Animator>();
@@ -42,42 +49,48 @@ public class PlayerFSMManager : MonoBehaviour {
         {
             fsm.enabled = false;
         }
-
+       
         states[newState].enabled = true;
         currentState = newState;
     }
     // Use this for initialization
     private void Start() {
 
-
         SetState(startState);
 
     }
 
-    
 
 
 	// Update is called once per frame
 	private void Update () {
-        xMove = Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime;
+        moveDirection = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, fallSpeed * Time.deltaTime);
+        //moveDirection = transform.TransformDirection(moveDirection);
+        //moveDirection *= moveSpeed;
+        //////////////////////////////////WALK//////////////////////////////////
         if (Input.GetAxisRaw("Horizontal") < 0)
         {
             mySpriteRenderer.flipX = true;
             animator.SetBool("isWalking", true);
-            transform.Translate(new Vector3(xMove, 0, 0));
-        }
-        if(Input.GetAxisRaw("Horizontal") > 0)
-        {
-            mySpriteRenderer.flipX = false;
+
+            transform.Translate(new Vector2(moveDirection.x, 0));
+        }                                                                     
+        if(Input.GetAxisRaw("Horizontal") > 0)                                
+        {                                                                     
+            mySpriteRenderer.flipX = false;                                   
             animator.SetBool("isWalking", true);
-            transform.Translate(new Vector3(xMove, 0, 0));
-        }
-        if(Input.GetAxisRaw("Horizontal") == 0)
+            transform.Translate(new Vector2(moveDirection.x, 0));
+        }                                                                     
+        if(Input.GetAxisRaw("Horizontal") == 0)                               
         {
             animator.SetBool("isWalking", false);
             SetState(PlayerState.IDLE);
         }
-        if(Input.GetKeyDown(KeyCode.LeftShift))
+        //////////////////////////////////WALK//////////////////////////////////
+
+
+        //////////////////////////////////RUN///////////////////////////////////
+        if (Input.GetKeyDown(KeyCode.LeftShift))
         {
             moveSpeed = 1.0f;
         }
@@ -85,9 +98,21 @@ public class PlayerFSMManager : MonoBehaviour {
         {
             moveSpeed = 0.5f;
         }
+        //////////////////////////////////RUN///////////////////////////////////
 
 
+        //////////////////////////////////JUMP///////////////////////////////////
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Jump!");
+            
+            transform.Translate(new Vector2(moveDirection.x, jump * Time.deltaTime));
+        }
+        //////////////////////////////////JUMP///////////////////////////////////
+
+        //moveDirection.y -= gravity * Time.deltaTime;    //ERROR
+        cc.Move(moveDirection * Time.deltaTime);
     }
-    
-  
+
+
 }
