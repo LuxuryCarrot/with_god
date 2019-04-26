89db4af6d2d8f7f2d1a59aae188a98b662d927fa
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-04-26 20:44:14 +0900

    ㅁㄴㅇㅁㄹㅁㄹㅇㅁㄴㄹㅇㅁ

---
+++ With_God_project/Assets/Script/SightScript/SightCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SightCheck : MonoBehaviour {
+
+    public float distance = 5.0f;
+    public LayerMask PlayerMask;
+    GameObject Player;
+    
+
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Physics2D.queriesHitTriggers = false;
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(3, 4), PlayerMask);
+
+        if (hit.collider != null && hit.collider.gameObject.tag == "Player")
+        {
+            Player.SetActive(false);
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(transform.position, new Vector2(3, 4));
+    }
+}
