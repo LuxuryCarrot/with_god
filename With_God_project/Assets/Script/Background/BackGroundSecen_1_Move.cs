5d24474b2a3d977fddc8d101527d46c4b92c3d70
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-04-26 23:14:59 +0900

    dd

---
+++ With_God_project/Assets/Script/Background/BackGroundSecen_1_Move.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundSecen_1_Move : MonoBehaviour {
+    //GameObject FrontBackGround[];
+    // Use this for initialization
+    private void Awake()
+    {
+        //FrontBackGround = GameObject.FindGameObjectsWithTag("FrontBackGround");
+    }
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
