0132347de6f9219aa32811fcb4b1be73467b38ff
laiklin <48722129+laiklin@users.noreply.github.com>
2019-05-06 22:57:33 +0900

    수정수정
    개 애니메이션 랜덤돌림 땅, 오브젝트 수정

---
+++ With_God_project/Assets/Script/Dog/DogExtra.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogExtra : MonoBehaviour
+{
+
+    GameObject Sight;
+
+    Animator ani;
+    int movementFlag = 0;
+
+    void Start()
+    {
+        ani = GetComponentInChildren<Animator>();
+        StartCoroutine("DogState");
+
+    }
+
+    IEnumerator DogState()
+    {
+        movementFlag = Random.Range(0, 2);
+
+        // idle
+        if (movementFlag == 0)
+        {
+            ani.SetInteger("FLAG", (int)movementFlag);
+        }
+        // eat
+        else if (movementFlag == 1)
+        {
+            ani.SetInteger("FLAG", (int)movementFlag);
+        }
+
+        yield return new WaitForSeconds(3f);
+
+        StartCoroutine("DogState");
+    }
+}
