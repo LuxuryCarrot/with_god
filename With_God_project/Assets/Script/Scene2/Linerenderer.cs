62df99812699848aac6a3e8935b0e9593f5530aa
laiklin <48722129+laiklin@users.noreply.github.com>
2019-05-30 02:14:37 +0900

    밧줄도전~~~~
    실패 ^-^ 힌지 ㅅㅂ~

---
+++ With_God_project/Assets/Script/Scene2/Linerenderer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Linerenderer : MonoBehaviour {
+
+    LineRenderer Line;
+    Vector3 box1Pos, box2Pos;
+
+    private void Start()
+    {
+        Line = GetComponent<LineRenderer>();
+        Line.startWidth = 0.1f;
+        Line.endWidth = 0.1f;
+
+        box1Pos = gameObject.GetComponent<Transform>().position;
+    }
+
+    private void Update()
+    {
+        Line.SetPosition(0, box1Pos);
+        Line.SetPosition(1, GameObject.Find("box2").GetComponent<Transform>().position);
+    }
+}
