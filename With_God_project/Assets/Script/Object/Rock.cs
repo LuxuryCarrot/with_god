03539f4f4de26986974b445ac5a65de3a3d76ff6
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-05-31 20:41:07 +0900

    zzz

---
+++ With_God_project/Assets/Script/Object/Rock.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rock : MonoBehaviour {
+    GameObject Stone;
+    GameObject Player;
+    GameObject StoneTower;
+
+	// Use this for initialization
+	void Start () {
+        Stone = GameObject.FindGameObjectWithTag("Stone");
+        Player = GameObject.FindGameObjectWithTag("Player");
+        StoneTower = GameObject.FindGameObjectWithTag("StoneTower");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
