c2bcf91711eb27d074106969239a11345029ee8d
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-06-02 01:48:54 +0900

    수레 수정

--- With_God_project/Assets/Script/Object/Stone.cs
+++ With_God_project/Assets/Script/Object/Stone.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Stone : MonoBehaviour {
     GameObject PlayerObj;
     GameObject StoneObj;
+
+    float PlayerForce = 0.0f;
 	// Use this for initialization
 	void Start () {
         PlayerObj = GameObject.FindGameObjectWithTag("Player");
@@ -14,6 +16,23 @@ public class Stone : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		
+        
 	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player" && Input.GetKey(KeyCode.DownArrow))
+        {
+            PlayerForce -= 0.1f;
+            Debug.Log("Down");
+        }
+        if (other.gameObject.tag == "Player" && Input.GetKey(KeyCode.UpArrow))
+        {
+            PlayerForce += 0.1f;
+            Debug.Log("Up");
+        }
+        if (other.gameObject.tag == "Player" && Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            transform.Translate(new Vector2(0.1f * 2.5f * Time.deltaTime, PlayerForce));
+        }
+    }
 }
--- With_God_project/Assets/Script/Object/Wagon.cs
+++ With_God_project/Assets/Script/Object/Wagon.cs
@@ -26,18 +26,19 @@ public class Wagon : MonoBehaviour {
     private void OnTriggerStay2D(Collider2D collision)
     {
 
-        bool v = (PlayerX >= (WagonX - 3.25f) && PlayerX <= (WagonX - 3.05f));
         bool m = (PlayerX <= (WagonX + 3.25f) && PlayerX >= (WagonX + 3.05f));
-        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.LeftControl) && v) 
-        {
-            Debug.Log("attached");
-            transform.Translate(new Vector2(1.35f * 1.95f * Time.deltaTime, 0));
-            
-        }
-        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.LeftControl) && m)
+
+        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.LeftControl) && m && Input.GetKey(KeyCode.LeftArrow))
         {
             Debug.Log("attached");
             transform.Translate(new Vector2(-1.35f * 1.95f * Time.deltaTime, 0));
+
+        }
+
+        if (collision.gameObject.tag == "Player" && Input.GetKey(KeyCode.LeftControl) && m && Input.GetKey(KeyCode.RightArrow))
+        {
+            Debug.Log("attached");
+            transform.Translate(new Vector2(1.35f * 1.95f * Time.deltaTime, 0));
 
         }
     }
