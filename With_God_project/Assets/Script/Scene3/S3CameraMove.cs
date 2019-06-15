d989c10ee700946c0449377e59d4d8d213ad3c51
laiklin <48722129+laiklin@users.noreply.github.com>
2019-06-15 23:21:26 +0900

    ㅠㅠ
    2씬 죽여

--- With_God_project/Assets/Script/Scene3/S3CameraMove.cs
+++ With_God_project/Assets/Script/Scene3/S3CameraMove.cs
@@ -2,7 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class S2CameraMove_Sub : MonoBehaviour {
+public class S3CameraMove : MonoBehaviour
+{
+    // 카메라 흔들림 효과
+    public float shakeTimer;
+    public float shakeAmount;
 
     float characterPosition = 5.0f;
 
@@ -24,10 +28,15 @@ public class S2CameraMove_Sub : MonoBehaviour {
 
     private void FixedUpdate()
     {
-        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x, ref velocity.x, smoothTimeX);
+        float posX = Mathf.SmoothDamp(transform.position.x, player.transform.position.x + characterPosition, ref velocity.x, smoothTimeX);
         float posY = Mathf.SmoothDamp(transform.position.y, player.transform.position.y, ref velocity.y, smoothTimeY);
 
         transform.position = new Vector3(posX, posY, transform.position.z);
+
+        if ((player.transform.position.x >= 52.0f) && (player.transform.position.x <= 240.0f))
+        {
+            ShakeCamera(0.1f, 0.08f);
+        }
 
         if (bounds)
         {
@@ -36,4 +45,20 @@ public class S2CameraMove_Sub : MonoBehaviour {
                 Mathf.Clamp(transform.position.z, minCameraPos.z, maxCameraPos.z));
         }
     }
+
+        private void Update()
+        {
+            if(shakeTimer >= 0)
+            {
+                Vector2 ShakePos = Random.insideUnitCircle * shakeAmount;
+                transform.position = new Vector3(transform.position.x + ShakePos.x, transform.position.y, transform.position.z);
+                shakeTimer -= Time.deltaTime;
+            }
+        }
+
+        public void ShakeCamera(float shakePwr, float shakeDur)
+        {
+            shakeAmount = shakePwr;
+            shakeTimer = shakeDur;
+        }
 }
