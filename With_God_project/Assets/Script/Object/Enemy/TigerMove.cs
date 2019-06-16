a32c5f5c261f19c4687dffb4c019dd5351415aac
LuxuryCarrot <dongkeun.lee@gmail.com>
2019-06-17 01:07:03 +0900

    dd

--- With_God_project/Assets/Script/Object/Enemy/TigerMove.cs
+++ With_God_project/Assets/Script/Object/Enemy/TigerMove.cs
@@ -8,44 +8,49 @@ public class TigerMove : MonoBehaviour
     GameObject enemy_check;
 
     GameObject Tiger;
-    GameObject Tiger_m;
-
+    GameObject godtree;
     Animator Tiger_ani;
 
     float speed;
-
+    Vector2 FinishPoint;
+    bool isTigerOnFinish;
 
     private void Awake()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
         enemy_check = GameObject.FindGameObjectWithTag("Wagon");
+        godtree = GameObject.FindGameObjectWithTag("Finish");
         // 태그 재활용..ㅎㅎ
 
+        FinishPoint = new Vector2(247, -4);
         Tiger = GameObject.FindGameObjectWithTag("rope");
-        Tiger_m = GameObject.FindGameObjectWithTag("rope_b");
 
+        isTigerOnFinish = false;
         Tiger_ani = GetComponent<Animator>();
 
-        speed = 2.0f;
-    }
-
-    void Start()
-    {
-        
     }
 
     void Update()
     {
-        if (Player.transform.position.x >= enemy_check.transform.position.x)
+        speed = 8.0f * Time.deltaTime;
+
+        if (Player.transform.position.x > godtree.transform.position.x + 10 && !isTigerOnFinish)
         {
+            transform.position = Vector2.MoveTowards(new Vector2(Tiger.transform.position.x, Tiger.transform.position.y), new Vector2(FinishPoint.x, FinishPoint.y), speed);
+        }
+        if (Tiger.transform.position.x == 247 && Tiger.transform.position.y == -4)
+        {
+            isTigerOnFinish = true;
+        }
+        if (!isTigerOnFinish)
+        {
+            Debug.Log("TigerStoped");
             Tiger_ani.SetBool("Moving", true);
-            transform.Translate(new Vector2(-1.1f * speed * Time.deltaTime, -0.35f * speed * Time.deltaTime));
         }
 
-        if (Tiger.transform.position.x <= 252)
+        if (isTigerOnFinish)
         {
             Tiger_ani.SetBool("Moving", false);
-            transform.Translate(new Vector2(0, 0));
         }
     }
 }
