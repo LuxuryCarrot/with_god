dbb93f3c2004d6230f387faff4e9d06ad3869717
laiklin <48722129+laiklin@users.noreply.github.com>
2019-06-16 22:35:45 +0900

    장산
    범

--- With_God_project/Assets/Script/Object/Enemy/TigerMove.cs
+++ With_God_project/Assets/Script/Object/Enemy/TigerMove.cs
@@ -6,6 +6,10 @@ public class TigerMove : MonoBehaviour
 {
     GameObject Player;
     GameObject enemy_check;
+
+    GameObject Tiger;
+    GameObject Tiger_m;
+
     Animator Tiger_ani;
 
     float speed;
@@ -14,12 +18,15 @@ public class TigerMove : MonoBehaviour
     private void Awake()
     {
         Player = GameObject.FindGameObjectWithTag("Player");
-        enemy_check = GameObject.FindGameObjectWithTag("rope");
+        enemy_check = GameObject.FindGameObjectWithTag("Wagon");
         // 태그 재활용..ㅎㅎ
+
+        Tiger = GameObject.FindGameObjectWithTag("rope");
+        Tiger_m = GameObject.FindGameObjectWithTag("rope_b");
 
         Tiger_ani = GetComponent<Animator>();
 
-        speed = 5.0f;
+        speed = 2.0f;
     }
 
     void Start()
@@ -29,10 +36,16 @@ public class TigerMove : MonoBehaviour
 
     void Update()
     {
-
         if (Player.transform.position.x >= enemy_check.transform.position.x)
         {
+            Tiger_ani.SetBool("Moving", true);
+            transform.Translate(new Vector2(-1.1f * speed * Time.deltaTime, -0.35f * speed * Time.deltaTime));
+        }
 
+        if (Tiger.transform.position.x <= 252)
+        {
+            Tiger_ani.SetBool("Moving", false);
+            transform.Translate(new Vector2(0, 0));
         }
     }
 }
